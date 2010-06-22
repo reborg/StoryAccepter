f1509637898de00a849df5c54a64be9ffda991d6
Adam Milligan <amilligan@pivotallabs.com>
2010-06-22 09:47:39 -0700

    Removed spurious libraries from targets.

--- Spec/UI/main.m
+++ Spec/UI/main.m
@@ -1,15 +1,7 @@
 #import <UIKit/UIKit.h>
 #import <Cedar-iPhone/Cedar.h>
-#import <Cedar-iPhone/CedarApplicationDelegate.h>
 
 int main(int argc, char *argv[]) {
-    // Builds that target the iPhone have to use Cedar as a static library.  Cedar includes an
-    // application delegate specific to tests, but since this code only instantiates it via
-    // interpolation and never by directly referencing the symbol the linker throws out the
-    // reference to the class.  So, this next line is to force the linker to keep the symbol
-    // around.
-    [[[CedarApplicationDelegate alloc] init] release];
-
     @try {
         NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
