2a12fdd22c4b5f79a65906e9f3c0b6b5074eb6bb
Adam Milligan <amilligan@pivotallabs.com>
2010-06-28 16:32:51 -0700

    Removed unnecessary exception handling.

--- Spec/UI/main.m
+++ Spec/UI/main.m
@@ -2,15 +2,9 @@
 #import <Cedar-iPhone/Cedar.h>
 
 int main(int argc, char *argv[]) {
-    @try {
-        NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-        int retVal = UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
-        [pool release];
-        return retVal;
-    } @catch (NSString *x) {
-        NSLog(@"=====================> NSString exception: %@", x);
-    } @catch (NSException *x) {
-        NSLog(@"=====================> NSException: %@", x);
-    }
+    int retVal = UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
+    [pool release];
+    return retVal;
 }
